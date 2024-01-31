d9058cc3d6077bfa3837ed4a3879c54ab3652b8b
unknown <astha2agarwal5@gmail.com>
2024-01-31 10:38:18 -0800

    Default Equals

---
+++ Java_Practice/Equals/Person.java
@@ -0,0 +1,28 @@
+//package Java_Practice.Equals;
+
+public class Person {
+    private String name;
+    private int age;
+    private String city;
+
+    public Person(String name,int age,String city)
+    {
+        this.name=name;
+        this.age=age;
+        this.city=city;
+    }
+    public static void main(String []args)
+    {
+        //Initialising a new object
+        Person person1= new Person("Astha",22,"Bengaluru");
+        //initialising a new object but with same contents
+        Person person2= new Person("Astha",22,"Bengaluru");
+        // Copying the refernce of one object to other
+        Person person3= person1;
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person2.equals(person3));
+        System.out.println(person1.equals(person3));
+
+    }
+}
