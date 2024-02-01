9e18acf6cf82fa907552cca5fa6b986cfc352799
unknown <astha2agarwal5@gmail.com>
2024-02-01 14:32:20 -0800

    Executor-Services

---
+++ Java_Practice/Executor_Service/ExecutorServices.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServices {
+    public static void main(String[] args) {
+        // Creating an ExecutorService with a fixed pool of 2 threads
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.submit(new count());
+        executorService.submit(new count());
+
+        // Shutting down the ExecutorService
+        executorService.shutdown();
+    }
+}
+
+class count implements Runnable {
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Task: " + i);
+        }
+    }
+}
