19645a862175642fab4127a7a11d743482410271
unknown <astha2agarwal5@gmail.com>
2024-01-31 16:23:27 -0800

    hashmap

---
+++ Java_Practice/Map_Collections/HashMaps.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMaps {
+    public static void main(String[] args) {
+
+        //Using hashmap to store fruits and their quantity
+        Map<String, Integer> fruits = new HashMap<>();
+        fruits.put("Melon", 1);
+        fruits.put("Mango", 3);
+        fruits.put("Peach", 5);
+        fruits.put("Lemon", 2);
+
+        //Retreiving the elements by key
+        System.out.println("The price of Melon is: " + fruits.get("Melon"));
+        System.out.println("The price of Mango is: " + fruits.get("Mango"));
+        System.out.println("The price of Peach is: " + fruits.get("Peach"));
+        System.out.println("The price of Lemon is: " + fruits.get("Lemon"));
+
+    }
+}
